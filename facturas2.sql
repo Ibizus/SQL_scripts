50f868813c9f60a6b529065a594faf724e59f303
Hector Lopez <hlopdia699@g.educaand.es>
2023-02-01 12:46:15 +0100

    Update facturas2.sql

--- facturas2.sql
+++ facturas2.sql
@@ -24,7 +24,7 @@ insert into clientes values (3, 'Luis','Ruiz','1995/01/19');
 
 insert into facturas values (1, '1997/08/08',3650, 1);
 insert into facturas values (2, '2022/11/09',4823, 2);
-insert into facturas values (3, '1997/12/10',675, 3);
+insert into facturas values (3, '1997/12/10',675, 1);
 insert into facturas values (4, '2023/01/15',985, 1);
 insert into facturas values (5, '1997/01/16',750, 2);
 insert into facturas values (6, '2023/01/17',536, 3);
@@ -53,7 +53,8 @@ select clientes.cod_cliente, nombre, apellido, sum(importe), timestampdiff(year,
 create table productos (
 cod_producto int primary key,
 nombre varchar(30),
-precio_unidad decimal(5,2)
+precio_unidad decimal(5,2),
+iva decimal(4,2)
 );
 
 create table lineas (
@@ -66,12 +67,12 @@ foreign key (cod_factura) references facturas(cod_factura),
 foreign key (cod_producto) references productos(cod_producto)
 );
 
-insert into productos values (1, 'Palé de ladrillos', 25);
-insert into productos values (2, 'Saco de cemento', 15);
-insert into productos values (3, 'Metro de arena', 35);
-insert into productos values (4, 'Carretilla', 125);
-insert into productos values (5, 'Metro de grava', 25);
-insert into productos values (6, 'Tela asfática', 55);
+insert into productos values (1, 'Palé de ladrillos', 25, 0.21);
+insert into productos values (2, 'Saco de cemento', 15, 0.21);
+insert into productos values (3, 'Metro de arena', 35, 0.16);
+insert into productos values (4, 'Carretilla', 125, 0.04);
+insert into productos values (5, 'Metro de grava', 25, 0.21);
+insert into productos values (6, 'Tela asfática', 55, 0.21);
 
 insert into lineas values (1,1,3,2);
 insert into lineas values (1,2,1,3);
@@ -80,6 +81,10 @@ insert into lineas values (1,3,2,5);
 insert into lineas values (2,1,4,2);
 insert into lineas values (2,2,5,2);
 insert into lineas values (2,3,6,2);
+
+insert into lineas values (3,1,4,5);
+insert into lineas values (3,2,5,6);
+insert into lineas values (3,3,6,7);
 
 
 /* Empezamos con join de DOS TABLAS: */
@@ -100,6 +105,34 @@ select clientes.cod_cliente, clientes.nombre, apellido, productos.nombre, produc
 select clientes.cod_cliente, clientes.nombre, clientes.apellido from clientes join facturas on clientes.cod_cliente = facturas.cod_cliente join lineas on lineas.cod_factura = facturas.cod_factura join productos on lineas.cod_producto = productos.cod_producto where productos.nombre like'%ladrillo%' and facturas.fecha_emision < '2001-01-01';
 
 /* Toda la información a nivel de cliente, facturas asociadas y líneas de factura con el detalle del producto e importe total de líneas */
+select clientes.cod_cliente, clientes.nombre, apellido, facturas.cod_factura, lineas.cod_linea, productos.cod_producto, productos.nombre, cantidad, concat(precio_unidad, ' €') as Precio_Unidad, concat(round(precio_unidad*cantidad,2), ' €') as Subtotal from clientes join facturas on clientes.cod_cliente = facturas.cod_cliente join lineas on facturas.cod_factura = lineas.cod_factura join productos on lineas.cod_producto = productos.cod_producto order by apellido asc, clientes.nombre asc;
+
+/* Mismo select anterior acortado con alias para cada tabla */
+select c.cod_cliente, c.nombre, apellido, f.cod_factura, l.cod_linea, p.cod_producto, p.nombre, cantidad, concat(precio_unidad, ' €') as Precio_Unidad, concat(round(precio_unidad*cantidad,2), ' €') as Subtotal from clientes as c join facturas as f on c.cod_cliente = f.cod_cliente join lineas as l on f.cod_factura = l.cod_factura join productos as p on l.cod_producto = p.cod_producto order by apellido asc, c.nombre asc;
+
+/* Datos de los clientes, datos de sus facturas, totales de sus facturas obtenidos de las lineas de factura */
+select c.cod_cliente, c.nombre, apellido, f.cod_factura, f.fecha_emision, concat(round(sum(precio_unidad*cantidad),2),' €') as Total from clientes as c join facturas as f on c.cod_cliente = f.cod_cliente join lineas as l on f.cod_factura = l.cod_factura join productos as p on l.cod_producto = p.cod_producto group by f.cod_factura;
+
+/* Datos de los clientes, datos de sus facturas, totales de sus facturas obtenidos de las lineas de factura, CUYO TOTAL ES MAYOR A 300€ */
+select c.cod_cliente, c.nombre, apellido, f.cod_factura, f.fecha_emision, concat(round(sum(precio_unidad*cantidad),2),' €') as Total from clientes as c join facturas as f on c.cod_cliente = f.cod_cliente join lineas as l on f.cod_factura = l.cod_factura join productos as p on l.cod_producto = p.cod_producto group by f.cod_factura having Total>300;
+
+/* Satos de los clientes y los totales de sus facturas obtenidos agrupando por cliente */
+select c.cod_cliente, c.nombre, apellido, concat(round(sum(precio_unidad*cantidad),2),' €') as Total from clientes as c join facturas as f on c.cod_cliente = f.cod_cliente join lineas as l on f.cod_factura = l.cod_factura join productos as p on l.cod_producto = p.cod_producto group by c.cod_cliente;
+
+/* Datos de los productos y totales de sus ventas */
+select p.cod_producto, p.nombre as Nombre, precio_unidad as 'Precio unit.', sum(cantidad) as 'Cant. vendida', concat(round(sum(precio_unidad*cantidad),2),' €') as Total from productos as p join lineas as l on p.cod_producto = l.cod_producto group by p.cod_producto;
+
+/* Adaptar la tabla de productos para que contemple un IVA 4%, 16%, 21% y tenerlo en cuante en el precio al hacer esta query: */
+/*  */
+select p.cod_producto, p.nombre as Nombre, precio_unidad as 'Precio unit.', sum(cantidad) as 'Cant. vendida', concat(round(sum(precio_unidad*(iva+1)*cantidad),2),' €') as 'Total con IVA' from productos as p join lineas as l on p.cod_producto = l.cod_producto group by p.cod_producto;
+
+
+
+
+
+
+
+
 
 
 
